84bdead810717e126b1be2db2e73f9e4a0d962b1
Karina <kechow@andrew.cmu.edu>
2012-10-21 18:29:16 -0400

    MOAR COMMENTS

--- go/src/P2-f12/contrib/tribimpl/trib-impl.go
+++ go/src/P2-f12/contrib/tribimpl/trib-impl.go
@@ -5,12 +5,13 @@ import (
 )
 
 type Tribserver struct {
-	//storagemaster
-	//hostport
-	//libstore
+	storagemaster string
+	hostport string
+	libstore *Libstore
 }
 
 func NewTribserver(storagemaster string, myhostport string) *Tribserver {
+	libstore := libstore.NewLibstore(storagemaster, myhostport, 0)
 
 	//pass storagemaster and hostport to new libstore with flags for debugging
 	//make new libstore
@@ -21,7 +22,7 @@ func NewTribserver(storagemaster string, myhostport string) *Tribserver {
 
 	//return the thing
 
-	return &Tribserver{}
+	return &Tribserver{storagemaster: storagemaster, hostpost: myhostport, libstore: libstore}
 }
 
 func (ts *Tribserver) CreateUser(args *tribproto.CreateUserArgs, reply *tribproto.CreateUserReply) error {
@@ -48,7 +49,7 @@ func (ts *Tribserver) AddSubscription(args *tribproto.SubscriptionArgs, reply *t
 
 	//check errors for any calls to libstore
 
-	//check if subscriptor exists
+	//check if subscriber exists
 	//then reply = NOSUCHUSER
 	//return nil
 
@@ -68,7 +69,7 @@ func (ts *Tribserver) RemoveSubscription(args *tribproto.SubscriptionArgs, reply
 
 	//check errors for any calls to libstore
 
-	//check if subscriptor exists
+	//check if subscriber exists
 	//then reply = NOSUCHUSER
 	//return nil
 
@@ -85,17 +86,77 @@ func (ts *Tribserver) RemoveSubscription(args *tribproto.SubscriptionArgs, reply
 }
 
 func (ts *Tribserver) GetSubscriptions(args *tribproto.GetSubscriptionsArgs, reply *tribproto.GetSubscriptionsReply) error {
+	
+	//check errors for any calls to libstore
+
+	//check if subscriber exists
+	//then reply = NOSUCHUSER, nil
+	//return nil
+
+	//check if user you are subscribing to
+	//then reply = NOSUCHTARGETUSER, nil
+	//return nil
+
+	//so if both exist
+	//then do get List (user:subscriptions, target)
+	//if there's no list, return an empty list
+	//then reply = OK, list
+	//return nil
+
+
 	return nil
 }
 
 func (ts *Tribserver) PostTribble(args *tribproto.PostTribbleArgs, reply *tribproto.PostTribbleReply) error {
+		
+	//check errors for any calls to libstore
+
+	//check if user exists
+	//if not
+	//then reply = NOSUCHUSER
+	//return nil
+
+	//else
+	//append to list([user:tribbles], tribbleID)
+	//put tribble [user:tribbleID] = tribble
+	//return nil
+
 	return nil
 }
 
 func (ts *Tribserver) GetTribbles(args *tribproto.GetTribblesArgs, reply *tribproto.GetTribblesReply) error {
+	
+	//check errors for any calls to libstore
+
+	//check if user exists
+	//if not
+	//then reply = NOSUCHUSER, nil
+	//return nil
+
+	//else
+	//getList(user:tribbles) => 
+  //for 100 tribbles (or up to 100 tribbles) at end of array (newest pushed to end) 
+  //  get(tribble ID) and push onto tribbles array
+  //reply = OK, tribbles array
+  //return nil
+
 	return nil
 }
 
 func (ts *Tribserver) GetTribblesBySubscription(args *tribproto.GetTribblesArgs, reply *tribproto.GetTribblesReply) error {
+	
+	//check errors for any calls to libstore
+
+	//check if user exists
+	//if not
+	//then reply = NOSUCHUSER, nil
+	//return nil
+
+	//else
+	//getList(user:subscriptions)
+	//for all users 
+	//	getTribbles for each user
+	//Go through all tribble lists, and create new list of most recent 100 tribbles 
+
 	return nil
 }
