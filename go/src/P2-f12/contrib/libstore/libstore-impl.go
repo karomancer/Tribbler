3129608c213e67f65bd3a7917fab3c51efe9b782
Liz Keller <sazanamisama@gmail.com>
2012-10-23 01:33:26 -0400

    libstore-impl done sans leasing

--- go/src/P2-f12/contrib/libstore/libstore-impl.go
+++ go/src/P2-f12/contrib/libstore/libstore-impl.go
@@ -10,16 +10,27 @@ import (
 	"strings"
 	"fmt"
 	"os"
+	"time"
+	"sort"
 )
 
 type Libstore struct {
 	cli *rpc.Client
 	nodelist []storageproto.Node
+	cache map [string]*rpc.Client
+	cacheM chan int
 	flags int
 	myhostport string
 }
 
-// Doesn't implement retry
+type nodeL []storageproto.Node
+func (nl nodeL) Len() int { return len(nl) }
+func (nl nodeL) Swap(i, j int) { nl[i], nl[j] = nl[j], nl[i]}
+
+type byID struct{ nodeL }
+func (c byID) Less(i, j int) bool { return c.nodeL[i].NodeID < c.nodeL[j].NodeID }
+
+
 func iNewLibstore(server string, myhostport string, flags int) (*Libstore, error) {
 	ls := &Libstore{}
 	ls.flags = flags
@@ -34,18 +45,33 @@ func iNewLibstore(server string, myhostport string, flags int) (*Libstore, error
 	ls.cli = cli
 
 	// Get list of storage servers from master storage server
-	args := storageproto.GetServersArgs{}
-	var reply storageproto.RegisterReply
-	err = cli.Call("StorageRPC.GetServers", args, &reply)
-	if err != nil {
-		return nil, err
+	// will retry five times after waiting for 1 second each try
+	triesLeft := 5
+	for triesLeft > 0 { 
+		args := storageproto.GetServersArgs{}
+		var reply storageproto.RegisterReply
+		err = cli.Call("StorageRPC.GetServers", args, &reply)
+		if err != nil {
+			return nil, err
+		}
+		if reply.Ready {
+			ls.nodelist = reply.Servers
+			break
+		}
+		triesLeft --
+		time.Sleep(1000000000)
 	}
-	if reply.Ready {
-		ls.nodelist = reply.Servers
-	}
+
 	if ls.nodelist == nil {
 		return nil, lsplog.MakeErr("Storage system not ready")
 	}
+
+	sort.Sort(byID{ls.nodelist})
+
+	//initialize the cache
+	ls.cache = make(map[string]*rpc.Client)
+	ls.cacheM = make(chan int, 1)
+	ls.cacheM <- 1
 
 	// do NOT connect to other storage servers here
 	// this should be done in a lazy fashion upon the first use
@@ -58,24 +84,57 @@ func (ls *Libstore) getServer(key string) (*rpc.Client, error) {
 	// Use beginning of key to group related keys together
 	precolon := strings.Split(key, ":")[0]
 	keyid := Storehash(precolon)
-	// Assuming 1 storage server so just taking first node
-	_ = keyid // use keyid to compile
-	// Doesn't implement rpc connection caching since there is only 1 node
+	//we gotta find out which dude we want to connect to
+	fmt.Printf("Finding node for key: %v\n", keyid)
+	var i int
+	for i = 0; i < len(ls.nodelist); i++ {
+		if ls.nodelist[i].NodeID > keyid {
+			break
+		}
+	}
+	nodeIndex := i
+	//make the connection
+	hostport := ls.nodelist[nodeIndex].HostPort
+	cli, err := rpc.DialHTTP("tcp", hostport)
+	if err != nil {
+		fmt.Printf("Could not connect to server %s, returning nil\n", hostport)
+		return nil, err
+	}
+	// rpc connection caching
 	// Don't forget to properly synchronize when caching rpc connections
-	cli := ls.cli
+	<- ls.cacheM
+	ls.cache[key] = cli
+	ls.cacheM <- 1
+
 	return cli, nil
 }
 
 
 func (ls *Libstore) iGet(key string) (string, error) {
+	fmt.Println("balh!")
 	wantlease := false
 	args := &storageproto.GetArgs{key, wantlease, ls.myhostport}
 	var reply storageproto.GetReply
-	cli, err := ls.getServer(key)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "error in get server\n")
-		return "", err
+
+
+	fmt.Printf("getting stuffs\n")
+
+	//check cache for key
+	<- ls.cacheM
+	fmt.Printf("hanging?\n")
+	node, ok := ls.cache[key]
+	ls.cacheM <- 1
+	//if it isn't found, we need to figure out which node to connect to
+	cli := node
+	var err error
+	if ok != true {
+		cli, err = ls.getServer(key)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error in get server\n")
+			return "", err
+		}
 	}
+
 	err = cli.Call("StorageRPC.Get", args, &reply)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "RPC failed: %s\n", err)
@@ -90,10 +149,22 @@ func (ls *Libstore) iGet(key string) (string, error) {
 func (ls *Libstore) iPut(key, value string) error {
 	args := &storageproto.PutArgs{key, value}
 	var reply storageproto.PutReply
-	cli, err := ls.getServer(key)
-	if err != nil {
-		return err
+
+	//check cache for key
+	<- ls.cacheM
+	node, ok := ls.cache[key]
+	ls.cacheM <- 1
+	//if it isn't found, we need to figure out which node to connect to
+	cli := node
+	var err error
+	if ok != true {
+		cli, err = ls.getServer(key)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error in get server\n")
+			return err
+		}
 	}
+
 	err = cli.Call("StorageRPC.Put", args, &reply)
 	if err != nil {
 		return err
@@ -108,10 +179,22 @@ func (ls *Libstore) iGetList(key string) ([]string, error) {
 	wantlease := false
 	args := &storageproto.GetArgs{key, wantlease, ls.myhostport}
 	var reply storageproto.GetListReply
-	cli, err := ls.getServer(key)
-	if err != nil {
-		return nil, err
+
+	//check cache for key
+	<- ls.cacheM
+	node, ok := ls.cache[key]
+	ls.cacheM <- 1
+	//if it isn't found, we need to figure out which node to connect to
+	cli := node
+	var err error
+	if ok != true {
+		cli, err = ls.getServer(key)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error in get server\n")
+			return nil, err
+		}
 	}
+
 	err = cli.Call("StorageRPC.GetList", args, &reply)
 	if err != nil {
 		return nil, err
@@ -125,10 +208,22 @@ func (ls *Libstore) iGetList(key string) ([]string, error) {
 func (ls *Libstore) iRemoveFromList(key, removeitem string) error {
 	args := &storageproto.PutArgs{key, removeitem}
 	var reply storageproto.PutReply
-	cli, err := ls.getServer(key)
-	if err != nil {
-		return err
+
+	//check cache for key
+	<- ls.cacheM
+	node, ok := ls.cache[key]
+	ls.cacheM <- 1
+	//if it isn't found, we need to figure out which node to connect to
+	cli := node
+	var err error
+	if ok != true {
+		cli, err = ls.getServer(key)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error in get server\n")
+			return err
+		}
 	}
+
 	err = cli.Call("StorageRPC.RemoveFromList", args, &reply)
 	if err != nil {
 		return err
@@ -142,10 +237,22 @@ func (ls *Libstore) iRemoveFromList(key, removeitem string) error {
 func (ls *Libstore) iAppendToList(key, newitem string) error {
 	args := &storageproto.PutArgs{key, newitem}
 	var reply storageproto.PutReply
-	cli, err := ls.getServer(key)
-	if err != nil {
-		return err
+
+	//check cache for key
+	<- ls.cacheM
+	node, ok := ls.cache[key]
+	ls.cacheM <- 1
+	//if it isn't found, we need to figure out which node to connect to
+	cli := node
+	var err error
+	if ok != true {
+		cli, err = ls.getServer(key)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error in get server\n")
+			return err
+		}
 	}
+
 	err = cli.Call("StorageRPC.AppendToList", args, &reply)
 	if err != nil {
 		return err
