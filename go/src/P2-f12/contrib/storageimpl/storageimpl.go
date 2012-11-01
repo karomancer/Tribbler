525c557f262648ee28153e2bfdeb5554dac23ab1
Liz Keller <sazanamisama@gmail.com>
2012-10-31 23:24:29 -0400

    stroage impl plus errors

--- go/src/P2-f12/contrib/storageimpl/storageimpl.go
+++ go/src/P2-f12/contrib/storageimpl/storageimpl.go
@@ -20,6 +20,11 @@ import (
 type Storageserver struct {
 	nodeid uint32
 	isMaster bool
+	numNodes int
+	nodeList []storageproto.Node
+	nodeListM chan int
+	nodeMap map[storageproto.Node]int
+	nodeMapM chan int
 }
 
 func reallySeedTheDamnRNG() {
@@ -45,25 +50,125 @@ func NewStorageserver(master string, numnodes int, portnum int, nodeid uint32) *
 		isMaster = false
 	}
 
+	ss.nodeList = []storageproto.Node{}
+	ss.nodeListM = make(chan int, 1)
+	ss.nodeListM <- 1
+
+	ss.nodeMap = make(map[storageproto.Node]int)
+	ss.nodeMapM = make(chan int, 1)
+	ss.nodeMapM <- 1
+
 	if isMaster == false {
+		//connect to the master server
+		var master *rpc.Client
+		var err error
+		for err != nil {
+			//keep retrying until we can actually conenct
+			master, err = rpc.DialHTTP("tcp", master)
+			time.Sleep(1000000000)	
+		}
+
+		//set up args for registering ourselves
 		args := storageproto.RegisterArgs{}
 		info := storageproto.Node{}
 		info.HostPort = portnum
 		info.NodeID = nodeid 
 		args.ServerInfo = info
 		reply := storageproto.RegisterReply{}
-		err := RegisterServer(args, reply)
-	} 
+
+		for err != nil || reply.Ready != true {
+			//call register on the master node with our info as the args. Kinda weird
+			err = master.Call("StorageRPC.RegisterServer", args, reply)
+			//keep retrying until all things are registered
+			time.Sleep(1000000000)
+		}
+
+		//gotta still set up some other shits
+		//like get list of servers from reply maybe?
+		//spec is pretty vague...
+		<- ss.nodeListM
+		ss.nodeList = reply.Servers
+		ss.nodelistM <- 1
+
+		//non master doesn't keep a node map cause fuck you
+
+	} else {
+		//otherwise we are the master so we have to do some other shits
+		//maybe these are the same shits as the other dudes got to do so maybe we don't gotta bother? WHAT WHO KNOWS
+
+		//set up numNodes:
+		if (numnodes == 0) {
+			numnodes = 1
+		}
+		ss.numNodes = numNodes;
+		<- ss.nodeListM
+		//append self to nodeList and put self in map
+		append(ss.nodeList, &Node{})//some shit here})
+		ss.nodeListM <- 1
+		<- ss.nodeMapM
+		ss.nodeMap[&Node{}] = 0 //someshithere}] = 0
+	}
 
 	return ss
 }
 
 // Non-master servers to the master
 func (ss *Storageserver) RegisterServer(args *storageproto.RegisterArgs, reply *storageproto.RegisterReply) error {
+	//called on master by other servers
+	//first check if that server is alreayd in our map
+	<- ss.nodeMapM
+	_, ok := ss.nodeMap[args.ServerInfo]
+	//if not we have to add it to the map and to the list
+	if ok != true {
+		//put it in the list
+		<- ss.nodeListM
+		append(ss.nodeList, args.ServerInfo)
+		//put it in the map w/ it's index in the list just cause whatever bro
+		//map is just easy way to check for duplicates anyway
+		ss.nodeMap[args.ServerInfo] = ss.nodeList.length()
+	} 
+
+	//check to see if all nodes have registered
+	if ss.nodeList.length() == ss.numNodes {
+		//if so we are ready
+		reply.Ready = true
+	} else {
+		//if not we aren't ready
+		reply.Ready = false
+	}
+
+	//send back the list of servers anyway
+	reply.Servers = ss.nodeList
+
+	//unlock everything
+	ss.nodeListM <- 1
+	ss.nodeMapM <- 1
+	//NOTE: having these two mutexes may cause weird problems, might want to look into just having one mutex that is used for both the 
+	//node list and the node map since they are baiscally the same thing anyway.
+
 	return nil
 }
 
 func (ss *Storageserver) GetServers(args *storageproto.GetServersArgs, reply *storageproto.RegisterReply) error {
+	//this is what libstore calls on the master to get a list of all the servers
+	//if the lenght of the nodeList is the number of nodes then we return ready and the list of nodes
+	//otherwise we return false for ready and the list of nodes we have so far
+	<- ss.nodeListM
+
+	//check to see if all nodes have registered
+	if ss.nodeList.length() == ss.numNodes {
+		//if so we are ready
+		reply.Ready = true
+	} else {
+		//if not we aren't ready
+		reply.Ready = false
+	}
+
+	//send back the list of servers anyway
+	reply.Servers = ss.nodeList
+
+	ss.nodeListM <- 1
+
 	return nil
 }
 
