26d1c31a12ebe2dcb2868ab38ce077f6194edae9
Liz Keller <sazanamisama@gmail.com>
2012-11-06 15:01:40 -0500

    removed print statements to test faster

--- go/src/P2-f12/contrib/storageimpl/storageimpl.go
+++ go/src/P2-f12/contrib/storageimpl/storageimpl.go
@@ -230,10 +230,10 @@ func NewStorageserver(master string, numnodes int, portnum int, nodeid uint32) *
 	rpc.Register(ss.srpc)
 	go ss.GarbageCollector()
 
-	fmt.Println("started new server")
+	/*fmt.Println("started new server")
 	fmt.Println(storageproto.Node{HostPort: "localhost:" + strconv.Itoa(portnum), NodeID: ss.nodeid})
 	fmt.Printf("master? %v\n", ss.isMaster)
-	fmt.Printf("numnodes? %v\n", ss.numNodes)
+	fmt.Printf("numnodes? %v\n", ss.numNodes)*/
 
 	return ss
 }
@@ -243,7 +243,7 @@ func (ss *Storageserver) RegisterServer(args *storageproto.RegisterArgs, reply *
 	//called on master by other servers
 	//first check if that server is alreayd in our map
 
-	fmt.Println("called register server")
+	//fmt.Println("called register server")
 
 	<- ss.nodeMapM
 	_, ok := ss.nodeMap[args.ServerInfo]
@@ -285,8 +285,8 @@ func (ss *Storageserver) RegisterServer(args *storageproto.RegisterArgs, reply *
 	//NOTE: having these two mutexes may cause weird problems, might want to look into just having one mutex that is used for both the 
 	//node list and the node map since they are baiscally the same thing anyway.
 
-	fmt.Println(reply.Servers)
-	fmt.Printf("ready? %v\n", reply.Ready)
+	//fmt.Println(reply.Servers)
+	//fmt.Printf("ready? %v\n", reply.Ready)
 
 	return nil
 }
@@ -295,7 +295,7 @@ func (ss *Storageserver) GetServers(args *storageproto.GetServersArgs, reply *st
 	//this is what libstore calls on the master to get a list of all the servers
 	//if the lenght of the nodeList is the number of nodes then we return ready and the list of nodes
 	//otherwise we return false for ready and the list of nodes we have so far
-	fmt.Println("called get servers")
+	//fmt.Println("called get servers")
 	<- ss.nodeListM
 	//fmt.Println("locked nodeList Mutex sucessfully")
 	//check to see if all nodes have registered
@@ -326,8 +326,8 @@ func (ss *Storageserver) GetServers(args *storageproto.GetServersArgs, reply *st
 
 	ss.nodeListM <- 1
 
-	fmt.Println(reply.Servers)
-	fmt.Printf("ready? %v\n", reply.Ready)
+	//fmt.Println(reply.Servers)
+	//fmt.Printf("ready? %v\n", reply.Ready)
 
 	return nil
 }
